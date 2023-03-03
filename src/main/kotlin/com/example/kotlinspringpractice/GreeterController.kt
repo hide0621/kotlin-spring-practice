c764d866763470c58253d5dfc3ba506e19259e55
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-03 13:41:36 +0900

    リクエストでjsonを送る方法を実装

--- src/main/kotlin/com/example/kotlinspringpractice/GreeterController.kt
+++ src/main/kotlin/com/example/kotlinspringpractice/GreeterController.kt
@@ -2,8 +2,11 @@ package com.example.kotlinspringpractice
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -19,5 +22,10 @@ class GreeterController {
     @GetMapping("/hello/{name}")
     fun helloPathValue(@PathVariable("name")name: String): HelloResponse {
         return HelloResponse("Hello $name")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hell ${request.name}")
     }
 }
---
+++ src/main/kotlin/com/example/kotlinspringpractice/HelloRequest.kt
@@ -0,0 +1,3 @@
+package com.example.kotlinspringpractice
+
+data class HelloRequest(val name: String)
