6ec5aca957b427cddf90500d19852681e0252b44
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-03 13:05:06 +0900

    クラスにリクエストマッピングアノテーションを付けて、メソッドのパスを階層化した

--- src/main/kotlin/com/example/kotlinspringpractice/HelloController.kt
+++ src/main/kotlin/com/example/kotlinspringpractice/HelloController.kt
@@ -3,11 +3,13 @@ package com.example.kotlinspringpractice
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("hello")
 class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/world")
     fun index(model: Model): String {
         model.addAttribute("message", "Hello World!")
         return "index"
