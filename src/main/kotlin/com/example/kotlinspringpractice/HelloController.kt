c532091a5db2fcb6c182003468137435f0c55048
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-03 13:00:06 +0900

    hello worldを表示させた

---
+++ src/main/kotlin/com/example/kotlinspringpractice/HelloController.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinspringpractice
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("message", "Hello World!")
+        return "index"
+    }
+
+}
